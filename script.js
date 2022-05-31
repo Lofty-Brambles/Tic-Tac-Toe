bf00428c8f4714c98fea1e5c972d372fa3a21f1d
Lofty | Brambles <prantikdas2004@gmail.com>
2022-05-31 22:57:41 +0530

    😕 Starts styling input components

--- script.js
+++ script.js
@@ -1,3 +1,53 @@
-const date = document.querySelector(".year");
-date.textContent = (new Date()).getFullYear();
+(function () {
 
+	const contents = document.querySelectorAll(".tab-content");
+	const tabLinks = document.querySelectorAll(".tab-links");
+	const allInputs = document.querySelectorAll( "input" );
+	const date = document.querySelector(".year");
+
+	onLoading();
+
+	function onLoading() {
+
+		date.textContent = (new Date()).getFullYear();
+
+		allInputs.forEach(input => {
+			input.addEventListener("focusin", e => {
+				e.currentTarget.parentNode
+					.querySelector("label").classList
+					.add("active");
+			});
+			input.addEventListener("focusout", e => {
+				const ele = e.currentTarget;
+				if (!ele.value) {
+					ele.parentNode.querySelector("label")
+						.classList.remove("active");
+				};
+			});
+		});
+
+		tabLinks.forEach(tabLink => {
+			tabLink.addEventListener("click", e => {
+
+				tabLinks.forEach(element => {
+					element.classList.remove("active");
+				});
+
+				contents.forEach(element => {
+					element.style.display = "none";
+				});
+
+				e.currentTarget.classList.add("active");
+				const id = e.currentTarget.id;
+				const opened = document.querySelector(`#${id}-menu`);
+				opened.style.display = "flex";
+
+				startMatch(id);
+
+			});
+		});
+		window.onload = document.querySelector("#pvc").click();
+
+		
+	};
+})();
